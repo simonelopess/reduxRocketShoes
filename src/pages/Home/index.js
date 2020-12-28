28cfeda2e5cf13fd6ec3265193f6fcf3bfc6f41a
Simone Lopes da Silva <SIMLSILVA@firjan.com.br>
2020-12-28 15:35:32 -0300

    Estilização do home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,78 @@
 import React from 'react';
+import {MdAddShoppingCart} from 'react-icons/md';
+
+import {ProductList} from './styles';
+
 
 export default function Home(){
-  return <div/>
+  return (
+    <ProductList>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-saucony-guide-iso-2-feminino/10/311-3110-010/311-3110-010_zoom2.jpg?ts=1591721433&ims=326x" alt="Tênis"></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-saucony-guide-iso-2-feminino/10/311-3110-010/311-3110-010_zoom2.jpg?ts=1591721433&ims=326x" alt="Tênis"></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-saucony-guide-iso-2-feminino/10/311-3110-010/311-3110-010_zoom2.jpg?ts=1591721433&ims=326x" alt="Tênis"></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-saucony-guide-iso-2-feminino/10/311-3110-010/311-3110-010_zoom2.jpg?ts=1591721433&ims=326x" alt="Tênis"></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-saucony-guide-iso-2-feminino/10/311-3110-010/311-3110-010_zoom2.jpg?ts=1591721433&ims=326x" alt="Tênis"></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src="https://static.netshoes.com.br/produtos/tenis-saucony-guide-iso-2-feminino/10/311-3110-010/311-3110-010_zoom2.jpg?ts=1591721433&ims=326x" alt="Tênis"></img>
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF"/> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  )
 }
